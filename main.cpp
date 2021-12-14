033f3e8e262bd3e1ce93cdb52100763d19be90ca
bing-dong <643252149@qq.com>
2021-12-14 19:03:17 +0800

    add auth related files

--- main.cpp
+++ main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include "wsdd.nsmap"
 #include "soapwsddProxy.h"
+#include "soapDeviceBindingProxy.h"
+#include "soapMediaBindingProxy.h"
+
+using namespace std;
 
 int main()
 {
-    struct wsddProxy wsddTest;
+    struct wsddProxy wsdd;
     struct wsdd__ProbeType req;
-    struct __wsdd__ProbeMatches rep;
+    struct __wsdd__ProbeMatches res;
     
-    wsddTest.soap_endpoint = "soap.udp://239.255.255.250:3702";
-    wsddTest.send_Probe(&req);
-    wsddTest.recv_ProbeMatches(rep);
-
-    for (int i = 0; i < rep.wsdd__ProbeMatches->__sizeProbeMatch; i++) {
-        std::cout << rep.wsdd__ProbeMatches->ProbeMatch->XAddrs << std::endl;
+    wsdd.soap_endpoint = "soap.udp://239.255.255.250:3702";
+    wsdd.send_Probe(&req);
+    wsdd.recv_ProbeMatches(res);
+    
+    for (int i = 0; i < res.wsdd__ProbeMatches->__sizeProbeMatch; i++) {
+        cout << res.wsdd__ProbeMatches->ProbeMatch->XAddrs << endl;
     }
+    
+    return 0;
 }
